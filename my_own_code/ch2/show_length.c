40be0155c018a4c35550ff9f52959fdf0866ee71
YoungLee <YoungLeeNENU@gmail.com>
2013-05-04 22:35:31 +0800

    newly added

---
+++ my_own_code/ch2/show_length.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len) {
+    int i;
+    for (i = 0; i < len; i++)
+        printf ("%.2x\n", start[i]);
+    printf ("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+int main(int argc, char *argv[])
+{
+    int c = 1;
+    float d = 1.0;
+    
+    /* show_bytes(); */
+    show_int(c);
+    show_float(d);
+    
+    return 0;
+}
